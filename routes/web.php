9640305dffa77479347eca7dec4eeb6336a7d265
axecore <aldirenaldy323@gmail.com>
2022-03-30 23:23:53 +0800

    nginx route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
-$proxy_url    = getenv('PROXY_URL');
-$proxy_schema = getenv('PROXY_SCHEMA');
+// $proxy_url    = getenv('PROXY_URL');
+// $proxy_schema = getenv('PROXY_SCHEMA');
 
-if (!empty($proxy_url)) {
-   URL::forceRootUrl($proxy_url);
-}
+// if (!empty($proxy_url)) {
+//    URL::forceRootUrl($proxy_url);
+// }
 
-if (!empty($proxy_schema)) {
-   URL::forceSchema($proxy_schema);
-}
+// if (!empty($proxy_schema)) {
+//    URL::forceSchema($proxy_schema);
+// }
+
+URL::forceRootUrl(getenv('PROXY_URL'));
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DatabaseSettingController;
