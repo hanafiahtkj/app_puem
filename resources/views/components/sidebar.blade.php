c1b0d9693b3ae964cd3dba51131416aa7c878e8e
Hanafi <hanafiahtkj@gmail.com>
2022-04-16 01:05:38 +0800

    #

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -11,19 +11,17 @@
       </a>
     </div>
     <ul class="sidebar-menu">
-      {{-- <li class="menu-header">Darbor</li> --}}
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->routeIs('dashboard')) ? 'active' : '' }}">
         <a href="{{ route('dashboard') }}" class="nav-link"><i class="fas fa-home"></i><span>Dasbor</span></a>
       </li>
-      {{-- <li class="menu-header">Aplikasi</li> --}}
-      <li class="nav-item dropdown">
+      <li class="nav-item dropdown {{ (request()->is('uem*')) ? 'active' : '' }}">
           <a href="#" data-toggle="dropdown" class="nav-link has-dropdown" aria-expanded="false"><i class="fas fa-folder"></i><span>Data UEM</span></a>
           <ul class="dropdown-menu">
-            <li class="nav-item"><a href="{{ route('uem.individu.index') }}" class="nav-link">Data Individu</a></li>
-            <li class="nav-item"><a href="{{ route('uem.usaha.index') }}" class="nav-link">Data Usaha</a></li>
+            <li class="nav-item {{ (request()->is('uem/individu*')) ? 'active' : '' }}"><a href="{{ route('uem.individu.index') }}" class="nav-link">Data Individu</a></li>
+            <li class="nav-item {{ (request()->is('uem/usaha*')) ? 'active' : '' }}"><a href="{{ route('uem.usaha.index') }}" class="nav-link">Data Usaha</a></li>
           </ul>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->routeIs('pasar-desa.index')) ? 'active' : '' }}">
         <a href="{{ route('pasar-desa.index') }}" class="nav-link"><i class="fas fa-shopping-bag"></i><span>Pasar Desa</span></a>
       </li>
       <li class="nav-item">
@@ -40,23 +38,23 @@
       <li class="nav-item">
         <a href="" class="nav-link"><i class="fas fa-chart-line"></i><span>Grafik</span></a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ (request()->routeIs('gis.index')) ? 'active' : '' }}">
         <a href="{{ route('gis.index') }}" class="nav-link"><i class="fas fa-map-marker-alt"></i></i><span>Gis</span></a>
       </li>
-      <li class="nav-item dropdown">
+      <li class="nav-item dropdown {{ (request()->is('master*')) ? 'active' : '' }}">
         <a href="#" data-toggle="dropdown" class="nav-link has-dropdown" aria-expanded="false"><i class="fas fa-folder"></i><span>Master Data</span></a>
         <ul class="dropdown-menu">
-          <li class="nav-item"><a href="{{ route('master.kecamatan.index') }}" class="nav-link">Kecamatan</a></li>
-          <li class="nav-item"><a href="{{ route('master.desa.index') }}" class="nav-link">Desa</a></li>
-          <li class="nav-item"><a href="{{ route('master.kategori-komoditas.index') }}" class="nav-link">Kategori Komoditas</a></li>
-          <li class="nav-item"><a href="{{ route('master.komoditas.index') }}" class="nav-link">Komoditas</a></li>
-          <li class="nav-item"><a href="{{ route('master.sub-komoditas.index') }}" class="nav-link">Sub Komoditas</a></li>
-          <li class="nav-item"><a href="{{ route('master.produk.index') }}" class="nav-link">Produk</a></li>
-          <li class="nav-item"><a href="{{ route('master.instansi-pembina.index') }}" class="nav-link">Instansi Pembina</a></li>
-          <li class="nav-item"><a href="{{ route('master.perizinan.index') }}" class="nav-link">Perizinan</a></li>
-          <li class="nav-item"><a href="{{ route('master.badan-usaha.index') }}" class="nav-link">Badan Usaha</a></li>
-          <li class="nav-item"><a href="{{ route('master.pendidikan.index') }}" class="nav-link">Pendidikan</a></li>
-          <li class="nav-item"><a href="{{ route('master.pengguna.index') }}" class="nav-link">Pengguna</a></li>
+          <li class="nav-item {{ (request()->is('master/kecamatan*')) ? 'active' : '' }}"><a href="{{ route('master.kecamatan.index') }}" class="nav-link">Kecamatan</a></li>
+          <li class="nav-item {{ (request()->is('master/desa*')) ? 'active' : '' }}"><a href="{{ route('master.desa.index') }}" class="nav-link">Desa</a></li>
+          <li class="nav-item {{ (request()->is('master/kategori-komoditas*')) ? 'active' : '' }}"><a href="{{ route('master.kategori-komoditas.index') }}" class="nav-link">Kategori Komoditas</a></li>
+          <li class="nav-item {{ (request()->is('master/komoditas*')) ? 'active' : '' }}"><a href="{{ route('master.komoditas.index') }}" class="nav-link">Komoditas</a></li>
+          <li class="nav-item {{ (request()->is('master/sub-komoditas*')) ? 'active' : '' }}"><a href="{{ route('master.sub-komoditas.index') }}" class="nav-link">Sub Komoditas</a></li>
+          <li class="nav-item {{ (request()->is('master/produk*')) ? 'active' : '' }}"><a href="{{ route('master.produk.index') }}" class="nav-link">Produk</a></li>
+          <li class="nav-item {{ (request()->is('master/instansi-pembina*')) ? 'active' : '' }}"><a href="{{ route('master.instansi-pembina.index') }}" class="nav-link">Instansi Pembina</a></li>
+          <li class="nav-item {{ (request()->is('master/perizinan*')) ? 'active' : '' }}"><a href="{{ route('master.perizinan.index') }}" class="nav-link">Perizinan</a></li>
+          <li class="nav-item {{ (request()->is('master/badan-usaha*')) ? 'active' : '' }}"><a href="{{ route('master.badan-usaha.index') }}" class="nav-link">Badan Usaha</a></li>
+          <li class="nav-item {{ (request()->is('master/pendidikan*')) ? 'active' : '' }}"><a href="{{ route('master.pendidikan.index') }}" class="nav-link">Pendidikan</a></li>
+          <li class="nav-item {{ (request()->is('master/pengguna*')) ? 'active' : '' }}"><a href="{{ route('master.pengguna.index') }}" class="nav-link">Pengguna</a></li>
         </ul>
       </li>
       <li class="nav-item dropdown">
