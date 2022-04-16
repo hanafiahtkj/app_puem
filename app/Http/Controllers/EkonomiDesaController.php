dd40064bc4544e7514be9adf44af548181f3c479
axecore <aldirenaldy323@gmail.com>
2022-04-16 23:19:02 +0800

    fix sub table format 1

--- app/Http/Controllers/EkonomiDesaController.php
+++ app/Http/Controllers/EkonomiDesaController.php
@@ -66,47 +66,53 @@ class EkonomiDesaController extends Controller
 
         foreach ($komoditas as $key1 => $value1) {
 
-            $sub_komoditas = SubKomoditas::leftJoin('format1', function($join) use ($request){
-                $join->on('sub_komoditas.id', '=', 'format1.id_sub_komoditas')
-                    ->where('format1.id_kecamatan', '=', $request->kec)
-                    ->where('format1.id_desa', '=', $request->des)
-                    ->where('format1.tahun', '=', $request->tahun);
-            })
-            ->leftJoin('produk', function($join) {
-                $join->on('format1.id_produk', '=', 'produk.id');
-            })
-            ->where('sub_komoditas.id_komoditas', $value1->id)
-            ->get();
+            $sub_komoditas = SubKomoditas::where('id_komoditas', $value1->id)->groupBy('id_komoditas')->get();
 
             foreach ($sub_komoditas as $key2 => $value2) {
+
+                $sub = SubKomoditas::leftJoin('format1', function($join) use ($request){
+                        $join->on('sub_komoditas.id', '=', 'format1.id_sub_komoditas')
+                                ->where('format1.id_kecamatan', '=', $request->kec)
+                                ->where('format1.id_desa', '=', $request->des)
+                                ->where('format1.tahun', '=', $request->tahun);
+                        })
+                        ->leftJoin('produk', function($join) {
+                            $join->on('format1.id_produk', '=', 'produk.id');
+                        })
+                        ->where('sub_komoditas.id_komoditas', $value1->id)->get();
                 
                 $html .= '<tr>';
                 $html .= '<td>'.($key1 + 1).'</td>';
                 $html .= '<td>'.$value1->nama_komoditas.'</td>';
-                $html .= '<tr>';
-                    $html .= '<td></td>';
-                    $html .= '<td>'.$value2->nama_sub_komoditas.'</td>';
-                    $html .= '<td class="text-center">'. ($value2->nama_produk ? $value2->nama_produk : '-') .'</td>';
-                    $html .= '<td class="text-center">'. ($value2->jenis_komoditas ? $value2->jenis_komoditas : '-') .'</td>';
-                    $html .= '<td class="text-center">'. ($value2->luas_lahan ? $value2->luas_lahan : '-') .'</td>';
-                    $html .= '<td class="text-center">'. ($value2->swasta_luas ? $value2->swasta_luas : '-') .'</td>';
-                    $html .= '<td class="text-center">'. ($value2->swasta_ton ? $value2->swasta_ton : '-') .'</td>';
-                    $html .= '<td class="text-center">'. ($value2->rakyat_luas ? $value2->rakyat_luas : '-') .'</td>';
-                    $html .= '<td class="text-center">'. ($value2->rakyat_hasil ? $value2->rakyat_hasil : '-') .'</td>';
-                    $html .= '<td class="text-center">'. ($value2->nilai_produk ? $value2->nilai_produk : '-') .'</td>';
-                    $html .= '<td class="text-center">'. ($value2->pemasaran_hasil ? $value2->pemasaran_hasil : '-') .'</td>';
-                  if (!$value2->jenis_komoditas) {
-                    $html .= '<td class="text-center">';
-                    $html .= '<a href="" class="btn btn-primary btn-md"><i class="fa fa-plus"></i></a>';
-                    $html .= '</td>';   
-                  }else{
-                    $html .= '<td class="text-center">';
-                    $html .= '<div class="btn btn-group">';
-                    $html .= '<a href="" class="btn btn-warning btn-md"><i class="fa fa-edit"></i></a>';
-                    $html .= '<a href="" class="btn btn-danger btn-md"><i class="fa fa-trash"></i></a>';
-                    $html .= '</div>';
-                    $html .= '</td>'; 
-                  }
+                
+                foreach ($sub as $key3 => $value3) {
+                        $html .= '<tr>';
+                        $html .= '<td></td>';
+                        $html .= '<td>'.$value3->nama_sub_komoditas.'</td>';
+                        $html .= '<td class="text-center">'. ($value3->nama_produk ? $value3->nama_produk : '-') .'</td>';
+                        $html .= '<td class="text-center">'. ($value3->jenis_komoditas ? $value3->jenis_komoditas : '-') .'</td>';
+                        $html .= '<td class="text-center">'. ($value3->luas_lahan ? $value3->luas_lahan : '-') .'</td>';
+                        $html .= '<td class="text-center">'. ($value3->swasta_luas ? $value3->swasta_luas : '-') .'</td>';
+                        $html .= '<td class="text-center">'. ($value3->swasta_ton ? $value3->swasta_ton : '-') .'</td>';
+                        $html .= '<td class="text-center">'. ($value3->rakyat_luas ? $value3->rakyat_luas : '-') .'</td>';
+                        $html .= '<td class="text-center">'. ($value3->rakyat_hasil ? $value3->rakyat_hasil : '-') .'</td>';
+                        $html .= '<td class="text-center">'. ($value3->nilai_produk ? $value3->nilai_produk : '-') .'</td>';
+                        $html .= '<td class="text-center">'. ($value3->pemasaran_hasil ? $value3->pemasaran_hasil : '-') .'</td>';
+                    if (!$value3->jenis_komoditas) {
+                        $html .= '<td class="text-center">';
+                        $html .= '<a href="" class="btn btn-primary btn-md"><i class="fa fa-plus"></i></a>';
+                        $html .= '</td>';   
+                    }else{
+                        $html .= '<td class="text-center">';
+                        $html .= '<div class="btn btn-group">';
+                        $html .= '<a href="" class="btn btn-warning btn-md"><i class="fa fa-edit"></i></a>';
+                        $html .= '<a href="" class="btn btn-danger btn-md"><i class="fa fa-trash"></i></a>';
+                        $html .= '</div>';
+                        $html .= '</td>'; 
+                    }
+                    }
+                     
+                    
                 $html .= '</tr>';
                 $html .= '<td></td>';
                 $html .= '<td></td>';
@@ -122,7 +128,66 @@ class EkonomiDesaController extends Controller
                 $html .= '</tr>';
     
 
+
             }
+
+            // $sub_komoditas = SubKomoditas::leftJoin('format1', function($join) use ($request){
+            //     $join->on('sub_komoditas.id', '=', 'format1.id_sub_komoditas')
+            //         ->where('format1.id_kecamatan', '=', $request->kec)
+            //         ->where('format1.id_desa', '=', $request->des)
+            //         ->where('format1.tahun', '=', $request->tahun);
+            // })
+            // ->leftJoin('produk', function($join) {
+            //     $join->on('format1.id_produk', '=', 'produk.id');
+            // })
+            // ->where('sub_komoditas.id_komoditas', $value1->id)
+            // ->get();
+
+            // foreach ($sub_komoditas as $key2 => $value2) {
+                
+            //     $html .= '<tr>';
+            //     $html .= '<td>'.($key1 + 1).'</td>';
+            //     $html .= '<td>'.$value1->nama_komoditas.'</td>';
+            //     $html .= '<tr>';
+            //         $html .= '<td></td>';
+            //         $html .= '<td>'.$value2->nama_sub_komoditas.'</td>';
+            //         $html .= '<td class="text-center">'. ($value2->nama_produk ? $value2->nama_produk : '-') .'</td>';
+            //         $html .= '<td class="text-center">'. ($value2->jenis_komoditas ? $value2->jenis_komoditas : '-') .'</td>';
+            //         $html .= '<td class="text-center">'. ($value2->luas_lahan ? $value2->luas_lahan : '-') .'</td>';
+            //         $html .= '<td class="text-center">'. ($value2->swasta_luas ? $value2->swasta_luas : '-') .'</td>';
+            //         $html .= '<td class="text-center">'. ($value2->swasta_ton ? $value2->swasta_ton : '-') .'</td>';
+            //         $html .= '<td class="text-center">'. ($value2->rakyat_luas ? $value2->rakyat_luas : '-') .'</td>';
+            //         $html .= '<td class="text-center">'. ($value2->rakyat_hasil ? $value2->rakyat_hasil : '-') .'</td>';
+            //         $html .= '<td class="text-center">'. ($value2->nilai_produk ? $value2->nilai_produk : '-') .'</td>';
+            //         $html .= '<td class="text-center">'. ($value2->pemasaran_hasil ? $value2->pemasaran_hasil : '-') .'</td>';
+            //       if (!$value2->jenis_komoditas) {
+            //         $html .= '<td class="text-center">';
+            //         $html .= '<a href="" class="btn btn-primary btn-md"><i class="fa fa-plus"></i></a>';
+            //         $html .= '</td>';   
+            //       }else{
+            //         $html .= '<td class="text-center">';
+            //         $html .= '<div class="btn btn-group">';
+            //         $html .= '<a href="" class="btn btn-warning btn-md"><i class="fa fa-edit"></i></a>';
+            //         $html .= '<a href="" class="btn btn-danger btn-md"><i class="fa fa-trash"></i></a>';
+            //         $html .= '</div>';
+            //         $html .= '</td>'; 
+            //       }
+            //     $html .= '</tr>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '<td></td>';
+            //     $html .= '</tr>';
+    
+
+            // }
 
           
 
